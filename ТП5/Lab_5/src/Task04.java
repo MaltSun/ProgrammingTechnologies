0674be39dfdb6c7f371c5729b42f2d93f38fd5f6
Солодкова Полина <polina.solodkova507@gmail.com>
2024-03-20 15:29:40 +0300

    fix: change method checkBrackets

--- ТП5/Lab_5/src/Task04.java
+++ ТП5/Lab_5/src/Task04.java
@@ -1,10 +1,52 @@
+import java.util.Scanner;
+
 public class Task04 {
     public static void main(String[] args) {
+        String input = "(a[+s)]-[(d+f)]*(d+e)";
 
+        if (checkBrackets(input)) {
+            System.out.println("Правильная строка");
+        } else {
+            System.out.println("Неправильная строка");
+        }
     }
-    static boolean RightLine(String line){
-        int count = 0;
-        String[] words = line.split("");
-        return true;
+
+    public static boolean checkBrackets(String str) {
+        int roundCount = 0;
+        int squareCount = 0;
+        boolean roundflag = false;
+        boolean squareflag = false;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch == '(') {
+                roundCount++;
+                roundflag = true;
+            } else if (ch == ')') {
+                if (!squareflag) {
+                    return false;
+                }
+                roundCount--;
+                if (roundCount < 0) {
+                    return false;
+                }
+                roundflag = false;
+            } else if (ch == '[') {
+                squareCount++;
+                squareflag = true;
+            } else if (ch == ']') {
+                if (!roundflag) {
+                    return false;
+                }
+                squareCount--;
+                if (squareCount < 0) {
+                    return false;
+                }
+               squareflag = false;
+            }
+        }
+
+        return roundCount == 0 && squareCount == 0;
     }
 }
